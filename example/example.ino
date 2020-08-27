4233d930652695dd0ce6398e720a49462c55c14a
Jean-Marc <jm.chiappa@gmail.com>
2020-08-27 07:37:30 +0200

    Modification de l'exemple

--- example/example.ino
+++ example/example.ino
@@ -1,18 +1,21 @@
-#include "GUIAssigner.h"
+#include "VariableAssigner.h"
+#include "HardwareSerial.h"
 
+HardwareSerial trace(D30,D16);
 bool toto=false;
 float r;
-Assign test(&Serial);
+Assign test(&trace);
 
 void setup(void) {
 	Serial.begin(115200);
+  trace.begin(230400);
 	Serial.println(toto);
 	test.AssignToVariable(0,&toto);
 	test.AssignToVariable(1,&r);
-	test.ReadCommand();
-	Serial.println(toto);
 }
 
+unsigned long t;
 void loop(void) {
-	
+  test.ReadCommand();
+  Serial.println(r);
 }
